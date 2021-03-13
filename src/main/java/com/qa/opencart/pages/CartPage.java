69595c69d7561e253930285e319bb9107fb66ebd
gaurav.burhade77@gmail.com <gaurav.burhade77@gmail.com>
2021-03-13 10:05:23 +0530

    modified code on PR review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,10 +1,20 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage 
 {
+	//By Locator
+	By un = By.id("username");
 
 	public CartPage()
 	{
 		System.out.println("Cart Page");
+	}
+	
+	//Page Actionss
+	public void clickonCart()
+	{
+		System.out.println("click on Cart");
 	}
 }
